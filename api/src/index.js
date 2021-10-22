ff29322ba8fdf4a1176aee9f3a3eaaec4a6afc4b
GuilhermeFG <guilhermefguimaraes23@gmail.com>
2021-10-22 08:11:03 -0300

    api brinks

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,84 @@
-import db from './db.js';
+import db from './db.js'
 import express from 'express'
 import cors from 'cors'
 
+
 const app = express();
-
-app.use(express());
 app.use(cors());
+app.use(express.json());
 
+app.get('/produto', async (req, resp) => {
+    try {
+        let a = await db.tb_produto.findAll({ order: [['id_produto', 'desc']]})
+        resp.send(a);
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
 
+app.post('/produto', async (req, resp) => {
+    try {
+        let {nome, categoria, precode, precopor, avaliacao, descricao, estoque, linkimg} = req.body;
+        let produtoOK = await db.tb_produto.findOne({ where: { nm_produto: nome}})
+        if(produtoOK !== null)
+            return resp.send({ erro: ' VIIIISH LEK PRODUTO JÀ INSERIDO'})
+        let r = await db.tb_produto.create({
+            nm_produto: nome,
+            ds_categoria: categoria,
+            vl_preco_de: precode,
+            vl_preco_por: precopor,
+            vl_avaliacao: avaliacao,
+            ds_produto: descricao,
+            qtd_estoque: estoque,
+            img_produto: linkimg,
+            bt_ativo: 1,
+            dt_inclusao: new Date()
+        })
+        resp.send(r);
 
-app.listen(process.env.PORT, x => console.log(`Server up at port ${process.env.PORT}`))
+        } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
+
+app.put('/produto/:id', async (req, resp) => {
+    try {
+        let {nome, categoria, precode, precopor, avaliacao, descricao, estoque, imgproduto, ativo, inclusao} = req.body;
+        let {id} = req.params;
+        let r = await db.tb_produto.update(
+            {
+                nm_produto: nome,
+                ds_categoria: categoria,
+                vl_preco_de: precode,
+                vl_preco_por: precopor,
+                vl_avaliacao: avaliacao,
+                ds_produto: descricao,
+                qtd_estoque: estoque,
+                img_produto: imgproduto,
+                bt_ativo: ativo,
+                dt_inclusao: inclusao
+            },
+            {
+                where: { id_produto: id }
+            }
+        )
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
+
+app.delete('/produto/:id', async (req, resp) => {
+    try {
+        let {id} = req.params;
+
+        let r = await db.tb_produto.destroy({
+            where: { id_produto: id}
+        })
+        resp.sendStatus(200)
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
+
+app.listen(process.env.PORT, x => console.log(`subiu na porta ${process.env.PORT}`));
---
+++ reactjs/src/service/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const api = axios.create({
+    baseURL: 'http://localhost:3030/'
+})
+
+export default class Api {
+    async listar(){
+        let r = await api.get('/produto');
+        return r.data;
+    }
+
+    async inserir(nome, categoria, avaliacao, precode, precopor, estoque, linkimg, descricao){
+        let r = await api.post('/produto', { nome, categoria, avaliacao, precode, precopor, estoque, linkimg, descricao } );
+        return r.data;
+    }
+
+    async alterar(id, nome, categoria, avaliacao, precode, precopor, estoque, linkimg, descricao) {
+       let r = await api.put('/produto/' + id, {nome, categoria, avaliacao, precode, precopor, estoque, linkimg, descricao});
+       return r.data;
+    }
+
+    async remover(id){
+        let r = await api.delete('/produto/' + id );
+        return r.data; 
+    }
+}
